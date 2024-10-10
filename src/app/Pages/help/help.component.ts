c10f69015cc70720772b540cebb1ad38fa84b75c
Marcos <80154421+M4rdom@users.noreply.github.com>
2024-10-10 19:07:47 +0200

    Added help and about pages

--- src/app/Pages/help/help.component.ts
+++ src/app/Pages/help/help.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FooterComponent } from '../../Structure/footer/footer.component';
+
 @Component({
   selector: 'app-help',
   standalone: true,
-  imports: [],
+  imports: [
+    MatExpansionModule,
+    FooterComponent,
+  ],
   templateUrl: './help.component.html',
   styleUrl: './help.component.css'
 })
