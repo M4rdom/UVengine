7daf14220f7a35c2e7870230db7c33ed1807e497
Marcos <80154421+M4rdom@users.noreply.github.com>
2024-10-11 13:11:55 +0200

    Added content con help and about page

--- src/app/Pages/help/help.component.ts
+++ src/app/Pages/help/help.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { FooterComponent } from '../../Structure/footer/footer.component';
 
@@ -8,6 +9,7 @@ import { FooterComponent } from '../../Structure/footer/footer.component';
   standalone: true,
   imports: [
     MatExpansionModule,
+    MatDividerModule,
     FooterComponent,
   ],
   templateUrl: './help.component.html',
