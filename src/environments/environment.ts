01d9f33641fc7eea3e6ba069f2b1ccd205c52f66
Marcos <80154421+M4rdom@users.noreply.github.com>
2024-10-12 08:54:30 +0200

    Edited backend url for proxy impl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,13 @@
 export const environment = {
     PRODUCTION: true,
     // Repository Manager
-    REPOSITORY_MANAGER_URL: 'http://repository-manager:5000',
+    REPOSITORY_MANAGER_URL: 'http://localhost/repository-manager',
         FEATURE_MODEL: 'FeatureModel',
         VERSIONS:'Versions',
         LIST_TEMPLATES: 'ListVersions',
     
     // UVEngine Resolver
-    UVENGINE_RESOLVER_URL: 'http://uvengine-resolver:5001',
+    UVENGINE_RESOLVER_URL: 'http://localhost/uvengine-resolver',
         RESOLVE: 'Resolver',
         
-    
-
 };
